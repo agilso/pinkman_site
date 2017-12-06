5d85f7c66f820032d389632c31ecfb1d6b0bad47
Agilso <agilso.b@gmail.com>
2017-12-06 17:00:58 -0200

    refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def escape str
+    CGI::escapeHTML(str)
+  end
+  
+  def to_do str
+    raw "<p class='bg-red white breathe-xs'><b>To do:</b> #{str}</p>"
+  end
+  
 end
